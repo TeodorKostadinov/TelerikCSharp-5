c3c5023e7ea6a0a268e6588e2552f0c9718be880
Teodor Kostadinov <teodor.kk.93@gmail.com>
2013-04-14 00:01:28 +0300

    1

---
+++ 2-RandomNums/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+//Write a program that generates and prints to
+//the console 10 random values in the range [100, 200].
+
+
+namespace _02.RandomGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random randomNumber = new Random();
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("{0}", randomNumber.Next(100, 201));
+            }
+        }
+    }
+}
---
+++ 3-DayWeek/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+//Write a program that prints to the console
+//which day of the week is today. Use System.DateTime.
+
+
+namespace _03.PrintDay
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime today = DateTime.Now;
+            Console.WriteLine("Today is {0}", today.DayOfWeek);
+        }
+    }
+}
